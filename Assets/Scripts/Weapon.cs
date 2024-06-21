713a6c50decc8b4fd30d6cb3018e9c16ff2bf6d5
Kenan Aliyev <kenanaliyev13@gmail.com>
2024-06-21 14:14:21 +0400

    Weapon Added

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] float enemyDetectionRadius = 5f;
+    [SerializeField] float attackReload = 1f;
+    [SerializeField] float rotationSpeed = 0.2f;
+
+    LayerMask enemyLayer;
+    float timeReload;
+    GameObject closestEnemy;
+
+    void Start()
+    {
+        enemyLayer = LayerMask.GetMask("Enemy");
+        timeReload = attackReload;
+    }
+
+    void Update()
+    {
+        Attack();
+    }
+
+    void Attack()
+    {
+        if (CheckForNearbyEnemies())
+        {
+            if (attackReload <= 0)
+            {
+                print("Attack");
+                Wait();
+            }
+            else
+                attackReload -= Time.deltaTime;
+        }
+    }
+
+    bool CheckForNearbyEnemies()
+    {
+        bool isEnemyNearby = false;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, enemyDetectionRadius, enemyLayer);
+        if (colliders.Length <= 0)
+            return isEnemyNearby;
+
+        float minDistance = Mathf.Infinity;
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.GetComponentInParent<SpriteRenderer>().enabled)
+            {
+                float distance = Vector3.Distance(transform.position, collider.transform.position);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    closestEnemy = collider.gameObject;
+                    isEnemyNearby = true;
+                }
+            }
+        }
+        if (isEnemyNearby)
+            Aim(closestEnemy.GetComponent<Collider2D>());
+        return isEnemyNearby;
+    }
+
+    void Aim(Collider2D enemy)
+    {
+        Vector3 direction = enemy.transform.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.Euler(0, 0, angle);
+        LeanTween.rotate(gameObject, rotation.eulerAngles, rotationSpeed);
+    }
+
+    void Wait()
+    {
+        attackReload = timeReload;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, enemyDetectionRadius);
+    }
+}
